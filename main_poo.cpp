8b22d3e12590d2f2c6b5bc5005bace256fe4343a
José García <pepofilms@gmail.com>
2022-12-14 21:20:36 -0500

    Feat: [Do Punteros]

---
+++ main_poo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+   
+    /* Utilización de Punteros
+    char letra= 'A';
+    char *puntero=&letra;
+    cout << (int *) &letra << endl;
+    cout << (int *)puntero<< endl;
+    cout << *puntero;*/
+}
