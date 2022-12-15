cde5f02c2222d5c5b786b34862530c653ed66daf
José García <pepofilms@gmail.com>
2022-12-14 23:07:00 -0500

    Feat:[Finish Struct]

--- main_poo.cpp
+++ main_poo.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main(){
-   
+struct Persona
+{
+    string nombre;
+    int edad;
+};
+int main()
+{
+    /*Uniones y enumeraciones*/
+    
+    /*Estructuras de datos en C+
+
+    // Persona p= Persona(); espacio tipo persona estructura
+    // Persona p = Persona();
+    // p.nombre="Diana";
+    // p.edad=26;
+    // cout << p.nombre << endl;
+    // cout << p.edad;
+
+    // Persona *p = new Persona(); en forma de puntero durante la ejecucion en forma dinamica la memoria
+
+    Persona *p = new Persona();
+    p->nombre = "Diana";
+    p->edad = 26;
+    cout << p->nombre << endl;
+    cout << p->edad << endl;
+    cout << &p << endl;
+    cout << &p->nombre << endl;
+    cout << &p->edad << endl;
+    cout << sizeof(p) << endl;
+    cout << sizeof(p->nombre) << endl;
+    cout << sizeof(p->edad) << endl;
+    cout << sizeof(Persona);
+
+    */
+
     /* Utilización de Punteros
     char letra= 'A';
     char *puntero=&letra;
